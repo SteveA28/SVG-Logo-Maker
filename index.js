2a689ffcc33ea56d3bc00172498aba54134b555f
SteveA28 <alonsosteven32@gmail.com>
2023-12-13 13:21:28 -0500

    added function to index

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const inquirer = require('inquirer');
 const { createSVG } = require('./lib/shapes');
+
+async function generateLogo() {
+    const userInput = await inquirer.prompt([
+      {
+        name: 'text',
+        message: 'Enter up to three characters:',
+        validate: input => input.length <= 3,
+      },
+      {
+        name: 'textColor',
+        message: 'Enter text color (keyword or hexadecimal):',
+      },
+      {
+        name: 'shape',
+        type: 'list',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+      {
+        name: 'shapeColor',
+        message: 'Enter shape color (keyword or hexadecimal):',
+      },
+    ]);
+  
+    createSVG(userInput);
+  
+    console.log('Generated logo.svg');
+  }
+  
+  generateLogo();
